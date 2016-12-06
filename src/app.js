924bcb3f010954bf5a2f59db2ab3a74836c00f1d
Curtis <curtis.allen.henson@gmail.com>
2016-12-06 15:38:28 -0500

    attempting to fix

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("src\madden-companion-exporter-firebase-adminsdk-vkseh-61d6626a08.json");
+const serviceAccount = require("madden-companion-exporter-firebase-adminsdk-vkseh-61d6626a08.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
